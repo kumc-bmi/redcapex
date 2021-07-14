46621be3a2ddbb5f202226e727234e46450a25b8
Lav Patel <contact.lavpatel@gmail.com>
2021-07-14 17:44:35 -0500

    able to export multiple projects from redcap itslef.

---
+++ Makefile
@@ -0,0 +1,25 @@
+run: venv
+	. venv/bin/activate && \
+	which python && \
+	mkdir -p ./export && \
+	# download which projects needs to export and its token && \
+	python download_redcap_data.py .env/REDCap_Export_Metadata_config.ini 22394 'https://redcap.kumc.edu/api/' && \
+	# converted downloaded csv with token into ini && \
+	python convert_csv_metadata_into_ini_format.py '.env/redcap_projects_exports.csv'  '.env/redcap_projects_exports.ini' && \
+	# download all listed redcap projects && \
+	python download_redcap_data.py .env/redcap_projects_exports.ini ALL 'https://redcap.kumc.edu/api/'
+
+
+venv: venv_clear
+	# "creating python virtual env"
+	python -m venv venv
+	. ./venv/bin/activate && \
+	pip install --upgrade pip  && \
+	pip install -r requirements.txt  && \
+	pip install -r requirements_dev.txt  && \
+	pip freeze >  requirements_pip_freeze.txt  && \
+	which pip && which python && python --version
+	
+venv_clear:
+	# "deleting python virtual env"
+	rm -rf venv
---
+++ convert_csv_metadata_into_ini_format.py
@@ -0,0 +1,46 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import pandas as pd
+from sys import argv
+
+
+def convert_csv_metadata_into_ini_format(input_csv_path, output_ini_path):
+
+    config_df = pd.read_csv(input_csv_path, dtype=str)
+    config_dict_lst = config_df.to_dict('records')
+
+    output = []
+    for config in config_dict_lst:
+        title = config['title']
+        output.append(f'[{title}]')
+        for key in config.keys():
+            value = config[key]
+            output.append(f'{key}:{value}')
+        output.append('\n')
+    output_string = "\n".join(output)
+
+    with open(output_ini_path, 'w', encoding='utf-8') as f:
+        f.write(output_string)
+
+
+# -
+if __name__ == "__main__":
+    print(f'argv: {argv}')
+
+    [input_csv_path, output_ini_path] = argv[1:]
+
+    convert_csv_metadata_into_ini_format(input_csv_path, output_ini_path)
