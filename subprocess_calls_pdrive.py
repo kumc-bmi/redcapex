201228c43b5627acfa0e79f9395c0574411f0845
lpatel <lpatel@sable.kumc.edu>
2021-10-22 14:54:25 -0500

    code review:  easy to read class and method names,

--- subprocess_calls_pdrive.py
+++ subprocess_calls_pdrive.py
@@ -1,37 +1,38 @@
 import os
 import subprocess
 
-class Subprocess_calls_pdrive:
-    def __init__(self):
+class NetworkDrive:
+    def __init__(self, p_Drive_path, p_Drive_mount_sable, p_Drive_domain, logging):
         ''' Constructor for this class. '''
         # Creating some member variables
-        self.p_Drive_path = os.environ.get('p_Drive_path')
-        self.p_Drive_mount_sable = os.environ.get('p_Drive_mount_sable')
-        self.p_Drive_domain = os.environ.get('p_Drive_domain')
+        self.p_Drive_path = p_Drive_path
+        self.p_Drive_mount_sable = p_Drive_mount_sable
+        self.p_Drive_domain = p_Drive_domain
+        self.logging = logging
 
-    def mount_Pdrive_sable(self, jnk_user, jnk_pass, logging):
+    def mount(self, jnk_user, jnk_pass):
         try:
             subprocess.check_call('sudo mount -t cifs %s %s -o user=%s,domain=%s,password=%s'%(self.p_Drive_path, self.p_Drive_mount_sable, jnk_user, self.p_Drive_domain, jnk_pass), shell=True)
         except Exception as e:
-            logging.error("""
-                The mounting was unsuccessfull with the exception:[Consult mount(8): manual for error codes e.g. 1= incorrect invocation, 64= partially unmounted, 0=success etc.]
+            self.logging.error("""
+                The mounting was unsuccessfull with the exception: 
                 %s
                 """ % (e)) 
 
-    def unmount_PDrive_sable(self, logging):
+    def unmount(self):
         try:
             subprocess.check_call('sudo umount -f %s'%(self.p_Drive_mount_sable), shell=True)
         except Exception as e:
-            logging.error("""
+            self.logging.error("""
                 The unmounting was unsuccessfull with the exception:
                 %s
                 """ % (e))
 
-    def copy_file_pdrive(self, logging, source_path, dest_path):
+    def copy_file(self, source_path, dest_path):
         try:
             subprocess.check_call('sudo cp -r -f %s %s'%(source_path, dest_path), shell=True) 
         except Exception as e:
-            logging.error("""
-                The copy of file from local ./export to P_drive was unsuccessfull with the exception:
+            self.logging.error("""
+                The copy of file from local %s to %s was unsuccessfull with the exception:
                 %s
-                """ % (e))
+                """ % (source_path, dest_path, e))
